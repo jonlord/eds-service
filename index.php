50fa1cb1071bee4bac962551e016ca067babf3fd
jonlord <jonlord748@gmail.com>
2015-06-03 16:20:31 +0200

    f

--- index.php
+++ index.php
@@ -33,77 +33,77 @@ $app->post('/execute/sold/store', function() use ($app) {
     });
 
 
-		$app->post('/execute/place/order', function() use ($app) {
-
-		  $post = $app->request->post();
-
-      $log = getLogger('executePlaceOrder');
-
-      $a = 'a';
-
-      //$order = file_get_contents("php://input");
-
-      //$order = $post['order'];
-
-			// $order = '{
-			// 								"order_number": "1001",
-			// 								"date": "2015-05-20",
-			// 								"items_count": "3",
-			// 								"items": [{
-			// 								"sku_id": "1",
-			// 								"product_reference": "XXX",
-			// 								"color_reference": "XXX",
-			// 								"quantity": "1",
-			// 								"size": "XXX",
-			// 								"purchase_price": "XXX"
-			// 								},
-			// 								{
-			// 								"sku_id": "2",
-			// 								"product_reference": "XXX",
-			// 								"color_reference": "XXX",
-			// 								"quantity": "1",
-			// 								"size": "XXX",
-			// 								"purchase_price": "XXX"
-			// 								},
-			// 						{
-			// 						"sku_id": "3",
-			// 						"product_reference": "XXX",
-			// 						"color_reference": "XXX",
-			// 						"quantity": "1",
-			// 						"size": "XXX",
-			// 						"purchase_price": "XXX"
-			// 						}]
-			// 					}';
-
-
-      //var_dump(file_get_contents("php://input"));
-
-			// $order_array = json_decode($order, true);
-      //
-			// $str_concat = "~" . $order_array['order_number'] . "~" . $order_array['date'] ."~";
-      //
-			// foreach ($order_array['items'] as $item) {
-      //
-			// 	foreach ($item as $key=>$value) {
-      //
-			// 		$str_concat .= $value . "~";
-			// 	}
-      //
-			// }
-
-		  //exec("WebOrder.exe $str_concat");
-      //exec("ElabSeco.bat $str_concat");
-
-
-      //$log->addNotice('Execute WebOrder with ' . $str_concat . ' at ' . date("Y/m/d h:i:sa"));
-
-		  $app->render(200,array(
-		                //'msg' => "File started: $data",
-		                //'msg' => $str_concat,
-                    'test' = $a;
-                    'msg' => $post;
-		            ));
-		    });
+		// $app->post('/execute/place/order', function() use ($app) {
+    //
+		//   $post = $app->request->post();
+    //
+    //   $log = getLogger('executePlaceOrder');
+    //
+    //   $a = 'a';
+    //
+    //   //$order = file_get_contents("php://input");
+    //
+    //   //$order = $post['order'];
+    //
+		// 	// $order = '{
+		// 	// 								"order_number": "1001",
+		// 	// 								"date": "2015-05-20",
+		// 	// 								"items_count": "3",
+		// 	// 								"items": [{
+		// 	// 								"sku_id": "1",
+		// 	// 								"product_reference": "XXX",
+		// 	// 								"color_reference": "XXX",
+		// 	// 								"quantity": "1",
+		// 	// 								"size": "XXX",
+		// 	// 								"purchase_price": "XXX"
+		// 	// 								},
+		// 	// 								{
+		// 	// 								"sku_id": "2",
+		// 	// 								"product_reference": "XXX",
+		// 	// 								"color_reference": "XXX",
+		// 	// 								"quantity": "1",
+		// 	// 								"size": "XXX",
+		// 	// 								"purchase_price": "XXX"
+		// 	// 								},
+		// 	// 						{
+		// 	// 						"sku_id": "3",
+		// 	// 						"product_reference": "XXX",
+		// 	// 						"color_reference": "XXX",
+		// 	// 						"quantity": "1",
+		// 	// 						"size": "XXX",
+		// 	// 						"purchase_price": "XXX"
+		// 	// 						}]
+		// 	// 					}';
+    //
+    //
+    //   //var_dump(file_get_contents("php://input"));
+    //
+		// 	// $order_array = json_decode($order, true);
+    //   //
+		// 	// $str_concat = "~" . $order_array['order_number'] . "~" . $order_array['date'] ."~";
+    //   //
+		// 	// foreach ($order_array['items'] as $item) {
+    //   //
+		// 	// 	foreach ($item as $key=>$value) {
+    //   //
+		// 	// 		$str_concat .= $value . "~";
+		// 	// 	}
+    //   //
+		// 	// }
+    //
+		//   //exec("WebOrder.exe $str_concat");
+    //   //exec("ElabSeco.bat $str_concat");
+    //
+    //
+    //   //$log->addNotice('Execute WebOrder with ' . $str_concat . ' at ' . date("Y/m/d h:i:sa"));
+    //
+		//   $app->render(200,array(
+		//                 //'msg' => "File started: $data",
+		//                 //'msg' => $str_concat,
+    //                 'test' = $a;
+    //                 'msg' => $post;
+		//             ));
+		//     });
 
         $app->get('/test', function() use ($app) {
 
